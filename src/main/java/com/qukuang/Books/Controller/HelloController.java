08e76119ecb00844c140e9c27cd794d83def5be7
QUKUANG <QUKUANG@DESKTOP-5N3VUVA>
2021-02-07 17:36:54 +0800

    2021.02.07测试上传

---
+++ src/main/java/com/qukuang/Books/Controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.qukuang.Books.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @RequestMapping(value="/hellobooks",method = RequestMethod.GET)
+    @ResponseBody
+    public String sayHello(){
+        return "Hello, World!Books";
+    }
+}
